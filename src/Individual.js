b580c1ccea4115e5d5967895aff08fa00d3ea396
manda <manda.li8686@gmail.com>
2023-11-16 14:16:31 -0600

    All functions now functional

--- src/Individual.js
+++ src/Individual.js
@@ -1,4 +1,6 @@
-import React from 'react';
+
+import React, { useState, useEffect } from 'react';
+
 import { useNavigate } from 'react-router-dom'; // Import useNavigate instead of useHistory
 
 const Individual = () => {
@@ -6,6 +8,30 @@ const Individual = () => {
   const goToMain = () => {
     navigate('/main'); // Navigate to Electricity page
   };
+
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState('');
+  const [charCount, setCharCount] = useState(0);
+const maxCharCount = 1000; 
+useEffect(() => {
+  const savedComments = localStorage.getItem('comments');
+  if (savedComments) {
+    setComments(JSON.parse(savedComments));
+  }
+}, []);
+const handleCommentChange = (e) => {
+  const value = e.target.value;
+setNewComment(value);
+setCharCount(value.length);
+};
+
+const handleCommentSubmit = () => {
+  const updatedComments = [...comments, newComment];
+  setComments(updatedComments);
+
+  localStorage.setItem('comments', JSON.stringify(updatedComments));
+  setNewComment('');
+};
   
   return (
     <div>
@@ -29,20 +55,62 @@ const Individual = () => {
 <ul>
     <li><strong>Mindful Portion Sizes:</strong> Take smaller portions to avoid leaving uneaten food on your plate. You can always go back for seconds if you're still hungry.
 </li>
+<li><strong>Limit Intake of Meat, Particularly Beef:</strong> Consider reducing the amount of meat in your diet, with a focus on consuming less beef, as its production is notably resource-intensive.</li>
+<li><strong>Look for Third-Party Certified Products:</strong> Seek out foods that have been certified by independent organizations, like those carrying the USDA Organic or Marine Stewardship Council (MSC) labels for seafood. These certifications often indicate more sustainable production practices.
+</li>
+<li><strong>Support Local Farmers:</strong> Shopping at local farmers' markets can lower the environmental impact associated with transporting food over long distances. Plus, it's a great way to enjoy fresher produce while supporting your local economy.</li>
 </ul>
-      <h2>Direct Communication and Collaboration with University Community</h2>
-      <ul>
-          <li>Hosts food waste tabling events for food waste and sustainability</li>
-          <li>Real time food waste data is displayed on TV screens in Rand</li>
-          <li>Vanderbilt supports innovation: The Greenfund competition winners of the 2022-2023 academic year proposed that the size of plates should be smaller to reduce food waste. Has been successfully implemented in EBI, and there are hopes to expand to other all-you-can-eat locations. </li>
-        </ul>
-        <h2>Landfill Diversion</h2>
+<br></br>
+      <h2>Vendor Choices</h2>
+      <div className='vendor'>
+      <a href='https://produceplace.com/' className="vendor-item">
+        <img src="local1.png" alt="Image 1"/>
+        <span>Produce Place</span>
+    </a>
+    <a href='https://eatatlocaltable.com/' className="vendor-item">
+        <img src="local2.png" alt="Image 2"/>
+        <span>Local Table</span>
+    </a>
+    <a href='https://www.wholefoodsmarket.com/' className="vendor-item">
+        <img src="local3.png" alt="Image 3"/>
+        <span>Whole Food Markets</span>
+    </a>
+    <a href='https://www.nashvillefarmersmarket.org/' className="vendor-item">
+        <img src="local4.png" alt="Image 4"/>
+        <span>Farmers Market</span>
+    </a>
+
+      </div>
+
+      <h2>Sustainability Leader Program</h2>
+      <p>Participation in the Sustainability Leaders program as a volunteer entails:</p>
         <ul>
-          <li>Trashcans were removed from Kissam and Rand in order to avoid having compostable containers sent to the landfill. If all items are returned to dish returns, the staff is trained in order for items to be properly sorted. </li>
-          <li>#7 on Princeton review for green colleges</li>
-          <li>Dining halls are zero waste: 90% landfill diversion </li>
-          <li>Excess food is donated to people in need in the community.</li>
+          <li>Attending monthly meetings during the academic year.
+</li>
+<li>Sustainability Leaders will take an active role in facilitating sustainable practices within their offices, departments, or residences by certifying these areas. They will also provide educational support and guidance in departmental and organizational meetings, organize outreach events and competitions, and set an example through their sustainable practices.</li>
         </ul>
+        <button className='learn'>Learn More</button>
+
+        <div className="discussion-board">
+        <h2 className='discuss'>Share Your Ideas</h2>
+        <div className='board-comment'>
+        <textarea 
+          value={newComment} 
+          onChange={handleCommentChange}
+          placeholder="Type your comment here..."
+        />
+        <span className="char-count">{`${charCount}/${maxCharCount}`}</span>
+        <br/>
+        <button className='submit' onClick={handleCommentSubmit}>Submit</button>
+        <ul>
+          {comments.map((comment, index) => (
+            <li key={index}>{comment}</li>
+          ))}
+        </ul>
+        </div>
+        
+      </div>
+        
         <button className='to-main' onClick={goToMain}>Go Back To MainPage</button>
       
       
